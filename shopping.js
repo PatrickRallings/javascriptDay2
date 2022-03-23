92a3266ca2f83de16b3dca694bd792d97dcfe592
Patrick Rallings <patrickrallings@Patricks-iMac.local>
2022-03-22 20:30:59 -0400

    Finished shopping assignment

--- shopping.js
+++ shopping.js
@@ -1,15 +1,34 @@
-let shopList = ['pop tarts', 'ramen noodles', 'chips', 'salsa', 'coffee']
-shopList.push('fruit loops')
+let shopList = ["pop tarts", "ramen noodles", "chips", "salsa", "coffee"];
+shopList.push("fruit loops");
 
-let index = shopList.indexOf('coffee')
-shopList[index] = 'fair trade coffee'
+let index = shopList.indexOf("coffee");
+shopList[index] = "fair trade coffee";
 
-index = shopList.indexOf('chips')
-shopList[index] = 'rice'
+index = shopList.indexOf("chips");
+shopList[index] = "rice";
 
-index = shopList.indexOf('salsa')
-shopList[index] = 'beans'
+index = shopList.indexOf("salsa");
+shopList[index] = "beans";
 
-console.log(shopList)
+let shopCart = [];
 
-let shopCart = []
+let listLength = shopList.length;
+let lastList = shopList[listLength - 1];
+
+shopCart.push(lastList);
+shopList.pop();
+
+while (shopList.length >= 1) {
+  listLength = shopList.length;
+  lastList = shopList[listLength - 1];
+  shopCart.push(lastList);
+  shopList.pop();
+}
+
+shopCart.sort();
+
+shopCart.reverse();
+
+let shopCartString = shopCart.toString()
+
+console.log(shopCartString);
